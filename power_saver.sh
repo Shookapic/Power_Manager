86bc403d5ca8799e1cb1b5440942c727a1a7ce08
shookx <cedric.roulof@epitech.eu>
2022-11-24 12:07:35 +0400

    Fixed for everyone

--- power_saver.sh
+++ power_saver.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+ip li 
+echo "Enter the id of the Network Card starting with \"wl\":"
+read id
 echo "Press 1 to turn ON Power Save mode or Press 2 to turn it OFF:"
 read choice
 if [ $choice -eq 1 ]
 then
-    sudo iw wlo1 set power_save on
-    sudo iw wlo1 get power_save
+    sudo iw $id set power_save on
+    sudo iw $id get power_save
 else
-    sudo iw wlo1 set power_save off
-    sudo iw wlo1 get power_save
+    sudo iw $id set power_save off
+    sudo iw $id get power_save
 fi
